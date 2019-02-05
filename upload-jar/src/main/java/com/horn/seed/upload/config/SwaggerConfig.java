2e94a394cf21d166fc43828261c95c204b275eb8
KUMAR Abhishek <abhishek@one.steria.dom>
2019-02-05 19:26:31 +0530

    uploading test setting

--- upload-jar/src/main/java/com/horn/seed/upload/config/SwaggerConfig.java
+++ upload-jar/src/main/java/com/horn/seed/upload/config/SwaggerConfig.java
@@ -1,9 +1,13 @@
 package com.horn.seed.upload.config;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -55,5 +59,25 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter {
 	      "urn:tos", ApiInfo.DEFAULT_CONTACT, "com.horn.seed.upload", "", new ArrayList<>());
 	    return apiInfo;
 	  }
+	  
+	/*  @Override
+	  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+	      converters.add(byteArrayHttpMessageConverter());
+	  }
+	   
+	  @Bean
+	  public ByteArrayHttpMessageConverter byteArrayHttpMessageConverter() {
+	      ByteArrayHttpMessageConverter arrayHttpMessageConverter = new ByteArrayHttpMessageConverter();
+	      arrayHttpMessageConverter.setSupportedMediaTypes(getSupportedMediaTypes());
+	      return arrayHttpMessageConverter;
+	  }
+	   
+	  private List<MediaType> getSupportedMediaTypes() {
+	      List<MediaType> list = new ArrayList<MediaType>();
+	      list.add(MediaType.IMAGE_JPEG);
+	      list.add(MediaType.IMAGE_PNG);
+	      list.add(MediaType.APPLICATION_OCTET_STREAM);
+	      return list;
+	  }*/
 
 }
--- upload-jar/src/main/java/com/horn/seed/upload/controller/ImageController.java
+++ upload-jar/src/main/java/com/horn/seed/upload/controller/ImageController.java
@@ -1,12 +1,12 @@
 package com.horn.seed.upload.controller;
 
 import java.io.IOException;
+import java.net.URL;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.mongodb.gridfs.GridFsResource;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.horn.seed.model.ImageDownload;
+import com.horn.seed.model.ImageUpload;
 import com.horn.seed.upload.service.ImageService;
 
 import io.swagger.annotations.Api;
@@ -40,9 +42,11 @@ public class ImageController {
 			@ApiResponse(code = 404, message = "The resource is not found") })
 	@PostMapping(value = "/upload" /* , headers = ("Content-Type=image/*") */ , consumes = {
 	"multipart/form-data" }, produces = MimeTypeUtils.APPLICATION_JSON_VALUE)
-	public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile multipartFile) throws IOException {
+	public ResponseEntity<ImageUpload> uploadImage(@RequestParam("file") MultipartFile multipartFile) throws IOException {
 		String id = imageService.uploadImage(multipartFile.getInputStream());
-		return new ResponseEntity<String>(id ,HttpStatus.OK);
+		ImageUpload imageUpload = new ImageUpload();
+		imageUpload.setId(id);
+		return new ResponseEntity<ImageUpload>(imageUpload, HttpStatus.OK);
 	}
 	
 	@ApiOperation(value = "To Get Image")
@@ -50,10 +54,10 @@ public class ImageController {
 			@ApiResponse(code = 401, message = "Not authorized to access the resource"),
 			@ApiResponse(code = 403, message = "Accessing the resource is forbidden"),
 			@ApiResponse(code = 404, message = "The resource is not found") })
-	@GetMapping(value = "/getImage" , consumes = {
-	"multipart/form-data" }, produces = MimeTypeUtils.APPLICATION_JSON_VALUE)
-	public ResponseEntity<InputStreamResource> getImage(@RequestParam("id") String id) throws IOException {
+	@GetMapping(value = "/getImage")
+	public ResponseEntity<byte[]> getImage(@RequestParam("id") String id) throws IOException {
 		GridFsResource gridFsResource = imageService.getImage(id);
-		return ResponseEntity.ok().contentType(MediaType.parseMediaType(gridFsResource.getContentType())).contentLength(gridFsResource.contentLength()).body(gridFsResource);
+		byte[] media = IOUtils.toByteArray(gridFsResource.getInputStream());
+		return new ResponseEntity<byte[]>(media, HttpStatus.OK);
 	}
 }
