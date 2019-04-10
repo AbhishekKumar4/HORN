4f2bc1c64764991c769e69feeb537d3017d1c6af
KUMAR Abhishek <kumarabhishek0388@gmail.com>
2019-04-10 19:58:42 +0530

    adding welcome component

--- frontend/angular-app/src/app/app.module.ts
+++ frontend/angular-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
