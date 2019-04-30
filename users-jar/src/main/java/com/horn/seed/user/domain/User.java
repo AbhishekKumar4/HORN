5f3eb5f930b5934f3c9e6b9d07df181458c483eb
KUMAR Abhishek <abhishek.kumar2@soprasteria.com>
2019-04-30 19:05:31 +0530

    addiing modification

--- users-jar/src/main/java/com/horn/seed/user/domain/User.java
+++ users-jar/src/main/java/com/horn/seed/user/domain/User.java
@@ -1,12 +1,22 @@
 package com.horn.seed.user.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "UserDetails")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String name;
 	private String userName;
--- users-jar/src/main/java/com/horn/seed/user/service/impl/UserServiceImpl.java
+++ users-jar/src/main/java/com/horn/seed/user/service/impl/UserServiceImpl.java
@@ -3,11 +3,13 @@ package com.horn.seed.user.service.impl;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.horn.seed.user.domain.User;
 import com.horn.seed.user.repository.UserRepository;
 import com.horn.seed.user.service.UserService;
 
+@Service
 public class UserServiceImpl implements UserService {
 
 	private UserRepository userRepository;
