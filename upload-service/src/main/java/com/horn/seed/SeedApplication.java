29d685bc43a51805383a4885de5a8b5468e09e89
KUMAR Abhishek <kumarabhishek0388@gmail.com>
2019-05-02 13:25:43 +0530

    removing unused code

--- upload-service/src/main/java/com/horn/seed/SeedApplication.java
+++ upload-service/src/main/java/com/horn/seed/SeedApplication.java
@@ -7,42 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Abhishek
  *
  */
-
-@SuppressWarnings("deprecation")
 @SpringBootApplication
-//@EnableSwagger2
-//@EnableWebMvc
-//@ComponentScan(basePackages = { "com.horn.seed.upload.config", "com.horn.seed.upload" })
-public class SeedApplication /*extends WebMvcConfigurerAdapter*/ {
-
-/*	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-
-	}
-
-	@Override
-	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-		converters.add(byteArrayHttpMessageConverter());
-	}
-
-	@Bean
-	public ByteArrayHttpMessageConverter byteArrayHttpMessageConverter() {
-		ByteArrayHttpMessageConverter arrayHttpMessageConverter = new ByteArrayHttpMessageConverter();
-		arrayHttpMessageConverter.setSupportedMediaTypes(getSupportedMediaTypes());
-		return arrayHttpMessageConverter;
-	}
-
-	private List<MediaType> getSupportedMediaTypes() {
-		List<MediaType> list = new ArrayList<MediaType>();
-		list.add(MediaType.IMAGE_JPEG);
-		list.add(MediaType.IMAGE_PNG);
-		list.add(MediaType.APPLICATION_OCTET_STREAM);
-		return list;
-		
-	}*/
+public class SeedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SeedApplication.class, args);
